a0debfc1a3e11ae3dd05a73cce7441301263da80
Mauricio Palma <mauricio.palma@sinnerschrader.com>
2018-03-15 15:12:00 +0100

    refactor(redis): include express server

--- redis-server.js
+++ redis-server.js
@@ -1,18 +1,28 @@
+const express = require('express');
+const http = require('http')
+const redis = require('redis');
 const WebSocket = require('ws');
-const redis = require("redis");
 
+const config = require('./config');
+const port = config.port + 1;
+
+const app = express();
+const server = http.createServer(app);
 const pub = redis.createClient();
-
-
-const wss = new WebSocket.Server({ port: 3001 });
+const wss = new WebSocket.Server({ server });
 // const channels = new Set();
 // let subs;
 
+const heartBeat = () => {
+  this.isAlive = true;
+}
+
 wss.on('connection', (ws, req) => {
+  ws.isAlive = true;
+  ws.on('pong', heartBeat);
 
   ws.on('message', (message) => {
     const redisMsg = JSON.parse(message);
-    console.log('it is connected', redisMsg)
     switch (redisMsg.action) {
       case 'SUBSCRIBE':
         // redisMsg.channels.forEach(channel => {
@@ -28,7 +38,6 @@ wss.on('connection', (ws, req) => {
         const subs = redis.createClient();
 
         subs.on('message', (channel, message) => {
-          console.log(channel, message);
           ws.send(JSON.stringify({
             action: 'SUBSCRIBEMSG',
             channel,
@@ -36,22 +45,33 @@ wss.on('connection', (ws, req) => {
           }));
         })
         subs.subscribe(redisMsg.channels.join(' '));
-        console.log(redisMsg.channels);
+        console.log(`> Subsbribed to: [${redisMsg.channels}]`);
         // if (toCloseSubs) {
         //   toCloseSubs.quit();
         // }
         break;
       case 'PUBLISH':
         redisMsg.channels.forEach(channel => {
-          // const wss  = channels.get(channel) || new Set();
-          // wss.forEach(ws => {
-
-          // })
           pub.publish(channel, redisMsg.message);
-
         });
-
+        break;
     }
   })
+});
 
+setInterval(() => {
+  wss.clients.forEach(ws => {
+    if (!ws.isAlive) {
+      console.log(`> terminate ws connection`);
+      return ws.terminate();
+    }
+    ws.ping(null, false, true);
+  });
+}, 3000);
+
+server.listen(port, () => {
+  console.log(`
+  > Redis - Websocket server is running ✌️
+  > Server started on port: ${server.address().port}
+  `);
 })
