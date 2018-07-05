3855d3ecf55cd5f5cbd8c86edda428b77071a61f
Mauricio Palma <mauricio.palma@sinnerschrader.com>
2018-07-05 18:27:51 +0200

    include typing for the index page

---
+++ components/active-link.tsx
@@ -0,0 +1,24 @@
+import { withRouter } from 'next/router';
+import { StyledLink } from './link';
+
+export interface ActiveLinkProps {
+  href?: string;
+  router?: any;
+  onClick?: React.MouseEventHandler<HTMLAnchorElement>;
+}
+
+const ActiveLink: React.SFC<ActiveLinkProps> = ({ children, router, href }) => {
+
+  const handleClick = (e: React.MouseEvent<HTMLAnchorElement>) => {
+    e.preventDefault()
+    router.push(href)
+  }
+
+  return (
+    <StyledLink href={href} onClick={handleClick}>
+      {children}
+    </StyledLink>
+  )
+}
+
+export default withRouter(ActiveLink)
