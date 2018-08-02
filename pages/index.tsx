4490434d785a9417affbe903875a72f43e027e22
Mauricio <mauricio.palma@sinnerschrader.com>
2018-08-02 13:23:05 +0200

    feat(speech-provider): remove recognition flag from the state object

--- pages/index.tsx
+++ pages/index.tsx
@@ -65,7 +65,7 @@ export default class Index extends React.Component<IndexProps, {}> {
         {wsBroker => (
           <SpeechProvider
             channel="Home"
-            wsBroker={wsBroker}>
+            wsBroker={wsBroker as any}>
             <SpeechContext.Consumer>
               {speech => (
                 <SpeechBroker
