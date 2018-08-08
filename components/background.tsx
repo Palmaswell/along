06fc16a2b4c317d7b3da590b78561535481e42c4
Mauricio <mauricio.palma@sinnerschrader.com>
2018-08-08 18:04:21 +0200

    feat(components): typings background

--- components/background.tsx
+++ components/background.tsx
@@ -1,14 +1,13 @@
 import styled from 'react-emotion';
-
 import colors from './colors';
 
 const StyledBackground = styled.div`
   min-width: 100vw;
   min-height: 100vh;
   background-color: ${colors.whiteSmoke()};
-`
+`;
 
-export const Background = ({ children }) => (
+export const Background: React.SFC<{}> = ({ children }) => (
   <StyledBackground>
     {children}
   </StyledBackground>
